e3858eac3c3ec3d15f5010df7ba45bd37b367f61
EduardoAlfres <eduardoafsm@gmail.com>
2026-03-08 13:17:05 -0300

    feat(RF002) Criação do componente da tela de Login

--- Trab-Web2/src/app/app.ts
+++ Trab-Web2/src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Login } from './login/login'; // importa o componente de Login
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true, // padrão para componentes exigido no PDF do trabalho
+  imports: [RouterOutlet, Login], // registra os componentes para uso
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
