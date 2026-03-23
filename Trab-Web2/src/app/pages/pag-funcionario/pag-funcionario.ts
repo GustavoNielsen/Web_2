78981ae962d1228da9fd8c86d05d6a914bd9f329
GustavoNielsen <gustavo.iassyl@gmail.com>
2026-03-23 09:32:25 -0300

    feat(RF11) Cria componente tela inicial funcionario

---
+++ Trab-Web2/src/app/pages/pag-funcionario/pag-funcionario.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pag-funcionario',
+  imports: [],
+  templateUrl: './pag-funcionario.html',
+  styleUrl: './pag-funcionario.css',
+})
+export class PagFuncionario {}
