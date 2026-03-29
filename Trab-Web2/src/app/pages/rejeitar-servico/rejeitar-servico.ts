b4e4dd78266464500157286bf6f8a86fd7319c4e
EduardoAlfres <eduardoafsm@gmail.com>
2026-03-29 20:04:39 -0300

    Refatoração RF007: Reformulação da estrutura do componente, reestilização usando o Bootstrap e validação

--- Trab-Web2/src/app/pages/rejeitar-servico/rejeitar-servico.ts
+++ Trab-Web2/src/app/pages/rejeitar-servico/rejeitar-servico.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core'; // output é necessário para emitir eventos para o componente pai
 import { CommonModule } from '@angular/common'; // Importante para diretivas básicas
 import { FormsModule } from '@angular/forms';   // necessário para quando o usuário digitar
 
@@ -8,24 +8,24 @@ import { FormsModule } from '@angular/forms';   // necessário para quando o usu
   imports: [CommonModule, FormsModule], // Adiciona suporte a formulários e funcionalidades básicas
   templateUrl: './rejeitar-servico.html',
   styleUrl: './rejeitar-servico.css',
-  encapsulation: ViewEncapsulation.None // permite que o bootstrap funcione
 })
+
 export class RejeitarServico {
-  motivoRejeicao: string = ""; // variável que armazena o motivo que o cliente digitar
+  motivoRejeicao: string = ""; // Variável que vai guardar o motivo da rejeição
 
-  // Exemplo de função que o html pode chamar futuramente para processar a rejeição
+  @Output() rejeicaoConfirmada = new EventEmitter<string>(); // Output marca a variavel como um evento que pode ser emitido para o componente pai, tambem passa o motivo da rejeição como string
+  @Output() rejeicaoCancelada = new EventEmitter<void>(); //EventEmitter é o que deixa o componente emitir eventos, nesse caso não precisa passar nada então é void
+
   confirmarRejeicao() {
-  // verifica se o cliente digitou um motivo de rejeição
-  if (this.motivoRejeicao.trim() === "") {
-    alert("Por favor, escreva o motivo da rejeição antes de confirmar.");
-    return; // Para a execução aqui e não mostra o próximo alert
+    alert("Serviço rejeiado.");
+    console.log("Estado da OS atualizado para Rejeitado");
+
+    this.rejeicaoConfirmada.emit(this.motivoRejeicao); // O emmit é o que dispara o evento, nesse caso ele passa o motivo da rejeição para o componente  pai
   }
 
-    // se o cliente digitou um motivo, é feita a rejeição
-    alert("Serviço rejeitado!"); // exibe uma mensagem de confirmação para o cliente
-
-    console.log("Serviço rejeitado pelo cliente.");
-    console.log("Motivo informado:", this.motivoRejeicao);
-    // No futuro aqui vai ter que fazer a chamada para o banco de dados
+  cancelar() {
+    alert("Rejeição cancelada.");
+    this.rejeicaoCancelada.emit(); //fecha o popup de rejeição
   }
+
 }
