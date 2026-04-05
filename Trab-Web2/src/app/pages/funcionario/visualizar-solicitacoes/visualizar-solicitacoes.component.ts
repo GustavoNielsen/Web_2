20e90dbae863f46faa9af2dccfb0aa8c32eb44d2
EduardoAlfres <eduardoafsm@gmail.com>
2026-04-05 02:17:20 -0300

    Feat: Implementação de visualizar ordem de serviço da home de funcionário

--- Trab-Web2/src/app/pages/funcionario/visualizar-solicitacoes/visualizar-solicitacoes.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-visualizar-solicitacoes',
-  imports: [],
-  templateUrl: './visualizar-solicitacoes.component.html',
-  styleUrl: './visualizar-solicitacoes.component.css',
-})
-export class VisualizarSolicitacoesComponent {}
