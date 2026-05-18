491d985e449c51e6dc5aa82dcf09d86680a9ebbe
MatheusGrigio <matheusgrigio.a@gmail.com>
2026-05-18 19:54:14 -0300

    removendo componente refatorado

--- Trab-Web2/src/app/pages/cliente/componente-integrador/componente-integrador.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-componente-integrador',
-  imports: [],
-  templateUrl: './componente-integrador.component.html',
-  styleUrl: './componente-integrador.component.css',
-})
-export class ComponenteIntegradorComponent {}
