2050d1a9ceef07c8834d6b13c38ea28df4c90994
MatheusGrigio <matheusgrigio.a@gmail.com>
2026-05-18 21:12:21 -0300

    fix PagCliente, alterando visualizar para abrir por rota de servico de id

--- Trab-Web2/src/app/pages/cliente/pag-cliente/pag-cliente.ts
+++ Trab-Web2/src/app/pages/cliente/pag-cliente/pag-cliente.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { VisualizarServico } from '../visualizar-servico/visualizar-servico';
 import { MostrarServico } from '../mostrar-servico/mostrar-servico';
 import { ResgatarServico } from '../resgatar-servico/resgatar-servico';
@@ -12,13 +13,16 @@ type ColunaOrdenacao = 'dataHora' | 'descricaoEquipamento' | 'estado';
 @Component({
   selector: 'app-pag-cliente',
   standalone: true,
-  imports: [CommonModule, VisualizarServico, MostrarServico, ResgatarServico, PagarServico],
+  imports: [CommonModule, RouterModule, VisualizarServico, MostrarServico, ResgatarServico, PagarServico],
   templateUrl: './pag-cliente.html',
   styleUrl: './pag-cliente.css',
 })
 export class PagCliente implements OnInit {
 
   private solicitacaoService = inject(SolicitacaoService);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+
 
   modal: string = '';
   solicitacaoSelecionada: any = null;
@@ -31,10 +35,30 @@ export class PagCliente implements OnInit {
     this.solicitacoes = this.solicitacaoService.listarTodos().sort((a, b) => 
       new Date(a.dataHora).getTime() - new Date(b.dataHora).getTime()
     );
+
+    this.route.paramMap.subscribe(params => {
+      const id = Number(params.get('id'));
+
+      if (id) {
+        this.abrirModalVisualizarPorId(id);
+      }
+    });
   }
 
   openModal(modal: string) {
     this.modal = modal;
+  }
+
+  abrirModalVisualizarPorId(id: number): void {
+    this.solicitacaoSelecionada = this.solicitacoes.find(s => s.id === id);
+
+    if (!this.solicitacaoSelecionada) {
+      alert('Solicitação não encontrada.');
+      this.router.navigate(['/cliente/home']);
+      return;
+    }
+
+    this.modal = 'visualizar';
   }
 
   visualizarServico(id: number, modal: string) {
@@ -94,7 +118,9 @@ export class PagCliente implements OnInit {
     });
   }
 
-  closeModal() {
+  closeModal(): void {
     this.modal = '';
+  this.solicitacaoSelecionada = null;
+  this.router.navigate(['/cliente/home']);
   }
 }
