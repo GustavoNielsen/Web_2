cfe9697ff57085cd8eb29f757fdb0d0c4d712f52
EduardoAlfres <eduardoafsm@gmail.com>
2026-05-31 05:08:32 -0300

    feat: integração da área do funcionário com a API REST e remoção do localStorage

--- Trab-Web2/src/app/pages/funcionario/crud-categoria/editar-categoria/editar-categoria.component.ts
+++ Trab-Web2/src/app/pages/funcionario/crud-categoria/editar-categoria/editar-categoria.component.ts
@@ -21,27 +21,24 @@ export class EditarCategoriaComponent {
   private route = inject(ActivatedRoute)
 
   ngOnInit(): void {
-    
     let id = +this.route.snapshot.params['id'];
-    // Com o id, obtém a pessoa
-    const res = this.categoriaService.buscarPorId(id);
-    if (res !== undefined)
-      this.categoria = res;
-    else
-      throw new Error ("Categoria não encontrada: id = " + id);
-
-}
+    this.categoriaService.buscarPorId(id).subscribe({
+      next: (res) => {
+        if (res) this.categoria = res;
+        else console.error("Categoria não encontrada: id = " + id);
+      },
+      error: (err) => console.error('Erro ao buscar categoria:', err)
+    });
+  }
 
 atualizar(): void {
-  if (this.formulario.form.valid) {
-    try {
-      this.categoriaService.atualizar(this.categoria);
-      this.router.navigate(['/funcionario/equipamento']);
-    } catch (error) {
-      console.error('Erro ao atualizar categoria:', error);
+    if (this.formulario.form.valid) {
+      this.categoriaService.atualizar(this.categoria).subscribe({
+        next: () => this.router.navigate(['/funcionario/equipamento']),
+        error: (err) => console.error('Erro ao atualizar categoria:', err)
+      });
+    } else {
+      console.log('Formulário inválido:', this.formulario.form.errors);
     }
-  } else {
-    console.log('Formulário inválido:', this.formulario.form.errors);
   }
-}
 }
